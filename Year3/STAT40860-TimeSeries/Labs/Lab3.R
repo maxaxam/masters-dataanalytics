d4633554208343fea054b72c6257d2f923856469
Milos Maksimovic <milosma@microsoft.com>
2016-10-24 22:25:27 +0200

    Remove xls data file and add CSV

--- Year3/STAT40860-TimeSeries/Labs/Lab3.R
+++ Year3/STAT40860-TimeSeries/Labs/Lab3.R
@@ -3,3 +3,11 @@
 
 library(forecast)
 library(lmtest)
+
+data <- read.csv("Data/lab3.csv", header = TRUE, sep = "\t")
+x1 <- data$Series.1
+x2 <- data$Series.2
+x3 <- data$Series.3
+x4 <- data$Series.4
+x5 <- data$Series.5
+x6 <- data$Series.6
